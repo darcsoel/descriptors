0dbb6be6c1fac52196bdd1506777079158cfb993
Ross <45120936+darcsoel@users.noreply.github.com>
2023-08-14 20:06:34 +0300

    Initial commit

---
+++ tests.py
@@ -0,0 +1,11 @@
+#  pylint:disable=missing-module-docstring
+#  pylint:disable=missing-function-docstring
+
+"""
+Unit tests
+"""
+
+
+def test_1() -> None:
+    """Dummy test to prevent exit code 5"""
+    assert True
