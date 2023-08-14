3514163cf4982cce2049a8af647edc7315a371fa
Ross <rostikvunarchuk@gmail.com>
2023-08-14 21:44:15 +0300

    format

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+# pylint: disable=missing-class-docstring
+
+
 class IntField:
     fields: dict[str, int] = {}
-    
-    def __init__(self, name) -> None:
+
+    def __init__(self, name: str) -> None:
         self.name = name
         self.fields[name] = None
-        
-    def __set__(self, obj, value: int):
+
+    def __set__(self, _: object, value: int) -> None:
         if value is None or isinstance(value, int):
             self.fields[self.name] = value
         else:
             raise ValueError("value must be int type")
-        
-    def __get__(self, obj, type = None):
+
+    def __get__(self, _: object, type_: object = None) -> int:
         return self.fields[self.name]
-    
+
 
 class Demo:
     field = IntField("field1")
     other = IntField("field2")
-    
-    def __init__(self, int_value) -> None:
+
+    def __init__(self, int_value: int) -> None:
         self.field = int_value
--- tests.py
+++ tests.py
@@ -6,15 +6,16 @@ Unit tests
 """
 
 import pytest
-from main import Demo, IntField
+
+from main import Demo
 
 
 def test_valid_value() -> None:
     dummy = Demo(2)
     assert dummy.field == 2
     assert dummy.other is None
-    
+
 
 def test_str_value() -> None:
     with pytest.raises(ValueError):
-        Demo("2")
+        Demo("2")  # type: ignore
